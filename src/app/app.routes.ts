62a40ccb981cbe7b3721b0f8a8a5e5b16159a84f
Marr03 <msiegut@g.educaand.es>
2025-04-27 16:42:14 +0200

    fix: limpiar rutas y establecer landing como inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import {LandingComponent} from './pages/landing/landing.component';
-
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingComponent },  // Landing será la página de inicio
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', component: LandingComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }, // Mantener tu home de usuario logueado
-  { path: '**', redirectTo: 'login' } // Redirige cualquier ruta desconocida
+  { path: '**', redirectTo: 'login' }  // Redirecciona cualquier ruta desconocida a Login
 ];
