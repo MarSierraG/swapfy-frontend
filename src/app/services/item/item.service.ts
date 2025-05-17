3d49fe5f484e8fdf161eace01f93e73a25303938
Marr03 <msiegut@g.educaand.es>
2025-05-17 11:25:44 +0200

    feat(services): implementar CRUD completo para artículos

--- src/app/services/item/item.service.ts
+++ src/app/services/item/item.service.ts
@@ -3,12 +3,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Item } from '../../models/item.model';
 import { environment } from '../../../environments/environment';
+import {ItemRequestDTO} from '../../models/item-request.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-  private apiUrl = `${environment.apiUrl}/items/available`;
+  private apiUrl = `${environment.apiUrl}/items`;
 
   constructor(private http: HttpClient) {}
 
@@ -19,7 +20,29 @@ export class ItemService {
   }
 
   getItemsByUserId(userId: number) {
-    return this.http.get<Item[]>(`http://localhost:1314/api/items/user/${userId}`);
+    return this.http.get<Item[]>(`${this.apiUrl}/user/${userId}`);
   }
+
+  getItemById(id: number) {
+    return this.http.get<Item>(`${this.apiUrl}/${id}`);
+  }
+
+  deleteItem(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  updateItem(id: number, dto: ItemRequestDTO): Observable<Item> {
+    return this.http.put<Item>(`${this.apiUrl}/${id}`, dto);
+  }
+
+  createItem(dto: any): Observable<Item> {
+    return this.http.post<Item>(`${this.apiUrl}`, dto);
+  }
+
+  getTags(): Observable<any[]> {
+    const url = `${environment.apiUrl}/tags`;
+    return this.http.get<any[]>(url);
+  }
+
 
 }
