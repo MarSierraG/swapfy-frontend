0a88d4dce97d4283bdf57b4e71e7578bf275f87c
MarSierraG <mariadelmarsierragutierrez32@gmail.com>
2025-08-19 16:25:05 +0200

    fix: desactivar modo mantenimiento y mostrar login normalmente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://swapfy-backend.onrender.com/api',
-  maintenance: true,
-  maintenanceMessage: 'Swapfy está en proceso de mejoras y no estará disponible temporalmente ¡Gracias por tu paciencia!'
+  maintenance: false,
+  maintenanceMessage: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:1314/api',
-  maintenance: true,
-  maintenanceMessage: 'Swapfy está en proceso de mejoras y no estará disponible temporalmente ¡Gracias por tu paciencia!'
+  maintenance: false,
+  maintenanceMessage: ''
 };
